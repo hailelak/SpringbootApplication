a1cbe9073dada3bd53f933284bcaecbcd426bd7e
Haile <haile_lak@yahoo.com>
2018-02-28 20:35:36 -0500

    pushing the files to upstream

---
+++ src/main/java/com/stratup/microservice/proofOfConcept/ProofOfConceptApplication.java
@@ -0,0 +1,13 @@
+package com.stratup.microservice.proofOfConcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProofOfConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProofOfConceptApplication.class, args);
+		System.out.println("Done!");
+	}
+}
---
+++ src/test/java/com/stratup/microservice/proofOfConcept/ProofOfConceptApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stratup.microservice.proofOfConcept;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProofOfConceptApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
